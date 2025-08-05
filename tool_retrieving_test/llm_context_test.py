afa682ef75345ee804c137b1c3533780efc5b7ac
Ilya Galyukshev <galyukshev.ilya@gmail.com>
2025-08-06 01:56:11 +0200

    update tmp

--- tool_retrieving_test/llm_context_test.py
+++ tool_retrieving_test/llm_context_test.py
@@ -31,7 +31,7 @@ from schemas import ToolSchema
 
 dotenv.load_dotenv()
 
-PROGRESS_FILE = Path("tmp_llm_context_progress_expanded.json")
+PROGRESS_FILE = Path("tmp_llm_context_progress_expanded_alibaba.json")
 
 
 class STEmbeddings(Embeddings):
